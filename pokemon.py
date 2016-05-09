9eb06333190d73fabfb63e3a60c497b7d07eb8b8
bman12three4 <byron.lathi@student.lovett.org>
2016-05-09 11:39:37 -0400

    Update pokemon.py
    Removed all of the functions, added in the fnc. to all of the references.

--- pokemon.py
+++ pokemon.py
@@ -5,20 +5,16 @@ import Adafruit_CharLCD as LCD
 import fnc as fnc
 
 lcd = LCD.Adafruit_CharLCDPlate()
-
-prompt = False
-clear = False
-counter = 0
   
 #Main Code
 lcd.clear()
-msgWait('Welcome to Text\nPokémon, 3)
-msgWait('Version 0.0.2')
-msgAnyKey('Press any key...')
-msgAnyKey('Welcome to your\nnew home!')
-msgAnyKey('Feel free to\nexplore!')
-msgAnyKeyMultiLine('Wait! Pokémon\nmay be hiding in','the grass!')
-msgAnyKeyMultiLine('I am professor\noak,','Pokémon Trainer')
-msgAnyKeyMultiLine('I am a pokemon\nresearcher.)
-msgAnyKeyMultiLine('Come to my Lab\nand let me show','you the world\nof PokéMon')
-travelling(2)
+fnc.msgWait('Welcome to Text\nPokémon, 3)
+fnc.msgWait('Version 0.0.2')
+fnc.msgAnyKey('Press any key...')
+fnc.msgAnyKey('Welcome to your\nnew home!')
+fnc.msgAnyKey('Feel free to\nexplore!')
+fnc.msgAnyKeyMultiLine('Wait! Pokémon\nmay be hiding in','the grass!')
+fnc.msgAnyKeyMultiLine('I am professor\noak,','Pokémon Trainer')
+fnc.msgAnyKeyMultiLine('I am a pokemon\nresearcher.)
+fnc.msgAnyKeyMultiLine('Come to my Lab\nand let me show','you the world\nof PokéMon')
+fnc.travelling(2)
