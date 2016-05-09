a98e0494c272d0531dedb1082915c538cb4d358f
bman12three4 <byron.lathi@student.lovett.org>
2016-05-09 11:37:53 -0400

    Update pokemon.py
    Still need to change the functions to be fnc.*

--- pokemon.py
+++ pokemon.py
@@ -2,63 +2,13 @@ import time
 import math
 
 import Adafruit_CharLCD as LCD
+import fnc as fnc
 
 lcd = LCD.Adafruit_CharLCDPlate()
 
 prompt = False
 clear = False
 counter = 0
-
-#Pauses the program until any of the keys are pressed.
-def anyKey():
-  prompt = True
-  while prompt:
-    if lcd.is_pressed(LCD.SELECT):
-     prompt = False
-    if lcd.is_pressed(LCD.UP):
-     prompt = False
-    if lcd.is_pressed(LCD.DOWN):
-      prompt = False
-    if lcd.is_pressed(LCD.LEFT):
-      prompt = False
-    if lcd.is_pressed(LCD.RIGHT):
-      prompt = False
-return
-
-#Lists one screen (two lines) and waits for any of the 5 keys to be pressed
-def msgAnyKey(str):
-  lcd.message(str)
-  anyKey()
-  lcd.clear()
-  
-#lists one screen and waits for the input seconds
-def msgWait(str, int):
-  lcd.message(str)
-  time.sleep(int)
-  lcd.clear()
-  
-#For messages with more than three liens
-def msgAnyKeyMultiLine(str1, str2):
-  msgAnyKey(str1)
-  lcd.show_cursor(True)
-  msgAnyKey(str2)
-  lcd.show_cursor(False)
-
-#says travelling followed by an elipses that grows and then deletes itself. Time is one second times input.
-def travelling(int):
-  counter = 0
-  while counter < int:
-    lcd.message('Travelling')
-    time.sleep(0.25)
-    lcd.message('.')
-    time.sleep(0.25)
-    lcd.message('.')
-    time.sleep(0.25)
-    lcd.message('.')
-    time.sleep(0.25)
-    lcd.clear()
-    counter = counter + 1
-
   
 #Main Code
 lcd.clear()
